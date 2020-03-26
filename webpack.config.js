f90402d0ab45c395471041f2f1e57f642c701303
shohei-yano <shohei-yano@cybozu.co.jp>
2020-03-26 13:43:18 +0900

    add some ignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode: 'development', // productionにしたい場合はコマンドラインで上書き可能
-  entry: { main: './src/js/index.js' },
+  entry: {main: './src/js/index.js'},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]_bundle.js'
