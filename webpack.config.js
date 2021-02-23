9607e093e097392a2e978dff522c845c95b9afed
Shohei Yano <y.idakko@gmail.com>
2021-02-24 01:12:08 +0900

    ignore trailing comma rule of prettier

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,15 @@ module.exports = (env, argv) => ({
   resolve: {
     alias: {
       modules: __dirname + "/node_modules",
-      common: __dirname + "/common",
-    },
+      common: __dirname + "/common"
+    }
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader"]
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -19,15 +19,15 @@ module.exports = (env, argv) => ({
           "style-loader",
           {
             loader: "css-loader",
-            options: { url: false },
+            options: { url: false }
           },
-          "sass-loader",
-        ],
+          "sass-loader"
+        ]
       },
       {
         test: /.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/i,
-        type: "asset",
-      },
-    ],
-  },
+        type: "asset"
+      }
+    ]
+  }
 });
