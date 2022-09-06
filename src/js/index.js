2595d954764abd743320079c7b4fda1b9567150c
yanoshi-1009 <y.idakko@gmail.com>
2022-09-06 15:14:22 +0900

    delete sample code

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,4 @@
 
 (() => {
   "use strict";
-
-  const test = "hello";
-  console.log(test);
 })();
-
-
-const promiseFunction = () => {return new Promise(~~)}
-
-const array1 = [0,1]
-
-array1.forEach(elm => {
-  promiseFunction()
-})
