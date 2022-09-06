7dd6e35070e96b68cec7a5dee33e42754f4971ad
yanoshi-1009 <y.idakko@gmail.com>
2022-09-06 15:11:31 +0900

    fix browsers list

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,16 @@
 
 (() => {
   "use strict";
+
+  const test = "hello";
+  console.log(test);
 })();
+
+
+const promiseFunction = () => {return new Promise(~~)}
+
+const array1 = [0,1]
+
+array1.forEach(elm => {
+  promiseFunction()
+})
