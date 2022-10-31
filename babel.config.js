a6cf051e0e48854d2a0615b6cf055071976a6fce
yanoshi-1009 <y.idakko@gmail.com>
2022-10-31 17:04:15 +0900

    versions import from package.json

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,23 @@
+const packageJson = require("./package.json");
+
 module.exports = function (api) {
   api.cache(true);
 
   const presets = [["@babel/preset-env"]];
   const plugins = [
-    ["@babel/plugin-transform-runtime", { version: "7.20.0" }],
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        version: packageJson.dependencies["@babel/runtime"].replace("^", "")
+      }
+    ],
     [
       "polyfill-corejs3",
       {
         method: "usage-pure",
-        version: "3.26"
+        version: packageJson.dependencies["core-js-pure"]
+          .replace(/^\^/g, "")
+          .replace(/.[0-9]+$/g, "")
       }
     ]
   ];
