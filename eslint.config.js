02128552d28effad24c91e1ac1d01016eed061bc
yanoshi-1009 <y.idakko@gmail.com>
2026-02-20 15:22:53 +0900

    fix(eslint): correct property name from 'ignore' to 'ignores'

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 import cybozuEslintConfig from "@cybozu/eslint-config/flat/presets/node-prettier.js";
 
-export default [...cybozuEslintConfig, { ignore: ["node_modules", "dist"] }];
+export default [...cybozuEslintConfig, { ignores: ["node_modules", "dist"] }];
