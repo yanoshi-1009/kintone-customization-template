502a8b42659feabec405e9d546c69fedb7dc43e7
yanoshi-1009 <y.idakko@gmail.com>
2026-05-19 17:14:34 +0900

    fix(eslint): add .js extension to import statement for compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
-import cybozuEslintConfig from "@cybozu/eslint-config/flat/presets/node-typescript-prettier";
+import cybozuEslintConfig from "@cybozu/eslint-config/flat/presets/node-typescript-prettier.js";
 
 export default [...cybozuEslintConfig, { ignores: ["node_modules", "dist"] }];
